a91d2397de3478f6e47fdf7d6ffc85b3c91823b7
Vanish <40368177+VanitoTM@users.noreply.github.com>
2023-07-30 17:19:53 +0200

    Tried to use vscode.dev cuz on holiday without my pc but very hard

---
+++ src/main/java/com/example/examplemod/LunarClientSpoofer.java
@@ -0,0 +1,7 @@
+package com.example.examplemod;
+
+
+public class LunarClientSpoofer {
+    //i will make it some day i promise 
+    //Made using vscode.dev - that was pain to even make that small code (read Package and class xD)
+}
